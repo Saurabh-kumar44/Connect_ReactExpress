93b7422b1e6763e62e2a37364325b6a8f604baab
saurav <sauravvermas1097v@gmail.com>
2023-03-02 16:33:25 +0530

    React Files

---
+++ reactFrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  const [students, setStudents] = useState([]);
+  
+  return (
+    <div className="App">
+      <h1>Connect React to Express</h1>
+    </div>
+  );
+}
+
+export default App;
