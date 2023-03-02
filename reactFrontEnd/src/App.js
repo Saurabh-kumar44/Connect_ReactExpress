edda1fe0efbf4fba1a911871ce21c9712142d1ab
saurav <sauravvermas1097v@gmail.com>
2023-03-02 16:45:04 +0530

    giving api to the axios

--- reactFrontEnd/src/App.js
+++ reactFrontEnd/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
 
 
 function App() {
   const [students, setStudents] = useState([]);
-  
+
+  useEffect(()=>{
+    async function getAllStudent(){
+      try{
+        const students = await axios.get("http://localhost:5000/api/student")//we only want to get the data so we'r using .get
+        console.log(students);
+      }catch(err){
+        console.log(err);
+      }
+    }
+  })
   return (
     <div className="App">
       <h1>Connect React to Express</h1>
